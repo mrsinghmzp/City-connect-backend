5b5e3a07d6e154ab0aba27d6fd92d336933c0bd7
Rahul Singh <rahul.cs6765@gmail.com>
2026-09-05 14:26:56 +0530

    Set trust proxy and add rate limiter middleware

--- CityConnect-Backend/src/app.js
+++ CityConnect-Backend/src/app.js
@@ -8,6 +8,9 @@ const path = require("path");
 
 const app = express();
 
+
+app.set("trust proxy", 1);
+app.use(limiter);
 // Security and utility middleware
 app.use(helmet({ crossOriginResourcePolicy: false })); // Allow serving static images across origins
 
